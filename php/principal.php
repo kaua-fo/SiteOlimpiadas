42f82f44f103b699518ff19f5122025e8fdbfd2c
kaua-fo <clouddigitallucas@gmail.com>
2024-09-12 20:01:52 -0300

    Atualizando css da página principal

--- php/principal.php
+++ php/principal.php
@@ -1,52 +1,52 @@
 <div class="container">
 <?php $listaNoticia = criarLista(); ?>
-        <br>
-        <h1><?php echo $titulo; ?></h1>
-        <h2><?= $sub_titulo ?></h2>
-            <?php foreach ($listaNoticia as $topico) { ?>
-                <section class="gridContainer">
-                    <div class="mainContent">
-                        <a class="pag-link" href="<?= $topico["href"] ;?>">
-                            <div class="categoryCard">
-                                <img alt="mainCardImg" class="mainCardImg" src="<?= $topico["imagem"] ;?>" width=320px height=180px>
-                                <p class="mainCategoryCardTitle"><?= $topico["titulo"] ;?></p>
-                                <p class="mainCategoryCardDescription"><?= reduzirStr($topico["descricao"], 200) ;?></p>
-                            </div>
-                        </a>
+    <br>
+    <h1><?php echo $titulo; ?></h1>
+    <h2><?= $sub_titulo ?></h2>
+    <section class="gridContainer">
+        <div class="mainContent">
+    <?php foreach ($listaNoticia as $topico) { ?>
+                <a class="pag-link" href="<?= $topico["href"] ;?>">
+                    <div class="categoryCard">
+                        <img src="<?= $topico["imagem"] ;?>" alt="mainCardImg" class="mainCardImg" width=320px height=180px>
+                        <p class="mainCategoryCardTitle"><?= $topico["titulo"] ;?></p>
+                        <p class="mainCategoryCardDescription"><?= reduzirStr($topico["descricao"], 200) ;?></p>
                     </div>
-            <?php } ?>
-                <aside class="sidebar">
-                    <div class="sidebarContent">
-                        <div class="IMC">
-                            <form class ="formImc">
-                                <div>
-                                    <p>Índice de massa corporal</p>
-                                </div>
-                                <div>
-                                    <div>
-                                        <br>
-                                        <label for="itext">Peso(Kg):</label>
-                                        <br>
-                                        <br>
-                                        <input name="itext" placeholder="Digite seu peso..." id="itext" type="text"/>
-                                        <br>
-                                        <br>
-                                    </div>
-                                    <div>
-                                        <label for="itext">Altura(M):</label>
-                                        <br>
-                                        <br>
-                                        <input name="itext" placeholder="Digite sua altura..." id="itext" type="text"/>
-                                        <br>
-                                        <br>
-                                    </div>
-                                    <div>
-                                        <input type="submit" value="Calcular"/>
-                                    </div>
-                                </div>
-                            </form>
-                    </div>
+                </a>
+    <?php } ?>
+        </div>
+        <aside class="sidebar">
+            <div class="sidebarContent">
+                <div class="IMC">
+                    <form>
+                        <div>
+                            <p>Índice de massa corporal</p>
                         </div>
-                </aside>
-                </section>
+                        <div>
+                            <div>
+                                <br>
+                                <label for="itext">Peso(Kg):</label>
+                                <br>
+                                <br>
+                                <input name="itext" placeholder="Digite seu peso..." id="itext" type="text"/>
+                                <br>
+                                <br>
+                            </div>
+                            <div>
+                                <label for="itext">Altura(M):</label>
+                                <br>
+                                <br>
+                                <input name="itext" placeholder="Digite sua altura..." id="itext" type="text"/>
+                                <br>
+                                <br>
+                            </div>
+                            <div>
+                                <input type="submit" value="Calcular"/>
+                            </div>
+                        </div>
+                    </form>
+            </div>
+                </div>
+        </aside>
+        </section>
 </div>
